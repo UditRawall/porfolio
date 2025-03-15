f8978920c59fda85020f08c152886eee1528e117
UditRawall <uditrawal209@gmail.com>
2025-03-15 20:58:00 +0530

    added email verifiation

--- src/component/pages/projects/Piclure.tsx
+++ src/component/pages/projects/Piclure.tsx
@@ -54,7 +54,7 @@ const Piclure = () => {
 
       <div className="flex w-full max-w-sm items-center space-x-2">
       <Input type="email" placeholder="Email" />
-      <Button type="submit">Subscribe</Button>
+      <Button type="submit">Subscribe to my email</Button>
     </div>
     </div>
   );
